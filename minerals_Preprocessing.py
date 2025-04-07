708cdbe85a6fd30e2aff5c03631768c68930e46e
agoerre <andreas.goerre@gmail.com>
2025-04-07 11:29:38 +0200

    Preprocessing complete for now

--- minerals_Preprocessing.py
+++ minerals_Preprocessing.py
@@ -47,13 +47,15 @@ df_clean['Mineral'] = df_clean['Mineral'].str.replace(r'\bvar\.\s*', '', regex=T
 df_clean['Mineral'] = df_clean['Mineral'].str.replace(r'\?\s*$', '', regex=True).str.strip() # Remove question mark at the end of the mineral name
 df_clean['Mineral'] = (df_clean['Mineral'].str.replace(r'\b(Group|Subgroup|Supergroup)\b', '', regex=True)
                        .str.replace(r'\s+', ' ', regex=True).str.strip()) # get rid of Group / Supergroup / Subgroup
+df_clean['Mineral'] = df_clean['Mineral'].str.replace(r'-\([^)]*\)', '', regex=True).str.strip() # get rid of the elements in parentheses as we don't analyse the dominant element further
+df_clean['Mineral'] = df_clean['Mineral'].str.replace(r'\s*\(TL\)', '', regex=True).str.strip() # get rid of (TL) parenthesis because it's another way to display a dominant element
 unique_minerals = df_clean['Mineral'].unique()
 unique_minerals = sorted(unique_minerals)
 
 print(f"Number of unique minerals: {df_clean['Mineral'].nunique()}")
 
 
-# df_clean.to_csv('clean_mindat.csv', index=False)
+df_clean.to_csv('clean_mindat.csv', index=False)
 
 # Filter minerals containing a hyphen
 hyphenated_minerals = df_clean[df_clean['Mineral'].str.contains('-', regex=False)]
@@ -63,3 +65,26 @@ hyphenated_list = hyphenated_minerals['Mineral'].unique().tolist()
 
 # Preview the list
 print(hyphenated_list)
+
+mineral_value_counts = df_clean['Mineral'].value_counts().reset_index()
+mineral_value_counts.columns = ['Mineral', 'Count']
+
+mineral_value_counts.to_csv('mineral_value_counts.csv', index=False)
+
+print(mineral_value_counts)
+
+# Create a new dataframe with the columns required for further analysis
+selected_columns = [
+    'Mindat Locality ID',
+    'Latitude & Longitude',
+    'Altitude',
+    'Type',
+    'Köppen climate type',
+    'Mineral'
+]
+
+df_selected = df_clean[selected_columns].copy()
+
+# Preview the result
+print(df_selected.head())
+df_selected.to_csv('data_for_analysis.csv', index=False)
